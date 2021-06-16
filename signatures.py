76119368193f4798646cd1efe0d130b273337c34
radleap <guy@radleap.com>
2021-06-15 20:56:05 -0400

    Completion of initial Signature.py file for asymmetric cryptography implementation

--- signatures.py
+++ signatures.py
@@ -1,35 +1,58 @@
 # signatures.py
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 # def generate_keys():
 # 	private = 'aa'
 # 	public = 'bb'
 # 	return private,public
 def generate_keys():
-    key = rsa.generate_private_key(public_exponent = 65537, key_size = 2048)
-
-    private_key = key.private_bytes(serialization.Encoding.PEM,
-                                    serialization.PrivateFormat.TraditionalOpenSSL,
-                                    serialization.NoEncryption()).decode('utf-8')
-
-    public_key = key.public_key().public_bytes(serialization.Encoding.OpenSSH,
-                                               serialization.PublicFormat.OpenSSH)
+    ''' This function uses RSA encryption with 2048 bits'''
+    private_key = rsa.generate_private_key(public_exponent = 65537, key_size = 2048)
+    public_key = private_key.public_key()
     return(private_key, public_key)
 
 
-def sign(message, private):
-	sig = 'agavraehga5e'
-	return sig
-def verify(message, sig, public):
-	return False
+# def sign(message, private):
+# 	sig = 'agavraehga5e'
+# 	return sig
+def sign(message, private_key):
+    signature = private_key.sign(message,
+                                 padding.PSS(
+                                 mgf = padding.MGF1(hashes.SHA256()),
+                                 salt_length = padding.PSS.MAX_LENGTH),
+                                 hashes.SHA256())
+    return(signature)
+
+
+
+# def verify(message, sig, public):
+# 	return False
+
+def verify(message, signature, public_key):
+    public_key = private_key.public_key() # need to refactor the argument of function and this line.
+    try:
+        public_key.verify(
+        signature,
+        message,
+        padding.PSS(
+        mgf=padding.MGF1(hashes.SHA256()),
+        salt_length=padding.PSS.MAX_LENGTH),
+        hashes.SHA256()
+    )
+        return(True)
+    except:
+        return(False)
+
 
  #prevents it from running if loaded the module... if just invoke it as a python script will run the tests
 if __name__=='__main__':
-	pr, pu = generate_keys()
+	private_key, public_key = generate_keys()
 	message = b"This is a secret message"
-	sig = sign(message, pr)
-	correct = verify(message, sig, pu)
+	signature = sign(message, private_key)
+	correct = verify(message, signature, public_key)
 
 	if correct:
 		print("success! Good sig")
